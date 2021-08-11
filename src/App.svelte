5e7e5812d7cd052b0edd27bc57d097b624fe895f
DariusCorvus <dariuscorvus@gmail.com>
2021-08-11 10:14:11 +0200

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import TailwindCss from "./TailwindCSS.svelte";
+	export let name;
+</script>
+
+<main class="text-center p-4 mx-0">
+	<h1 class="uppercase text-8xl font-thin"><span>Hello</span> <span class="text-blue-400">{name}!</span></h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		max-width: 240px;
+	}
+
+	h1 {
+		color: #ff3e00;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",  // JIT stands for Just in Time Compliation and makes the Build and Startup so fast.
+                // at the moment JIT is just in the preview but works like a charme.
+  purge: ['./public/index.html', './src/**/*.svelte'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
