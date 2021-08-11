14a96c755453006f7adf14240d1368a07c70db57
DariusCorvus <dariuscorvus@gmail.com>
2021-08-11 10:49:47 +0200

    spell(comments): gramma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mode: "jit",  // JIT stands for Just in Time Compliation and makes the Build and Startup so fast.
+  mode: "jit",  // JIT stands for Just in Time Compiler and makes the Build and Startup so fast.
                 // at the moment JIT is just in the preview but works like a charme.
   purge: ['./public/index.html', './src/**/*.svelte'],
   darkMode: false, // or 'media' or 'class'
